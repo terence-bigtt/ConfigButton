a9c1d4611644e14f5ebf57ad83afc214c94ea323
terence-bigtt <teereence@gmail.com>
2018-02-12 21:08:50 +0100

    Update ConfigButton.cpp
    added click handling

--- ConfigButton.cpp
+++ ConfigButton.cpp
@@ -6,71 +6,72 @@ void log(std::string msg){
   }
 }
 
-CallbackDuration::CallbackDuration(){
+Callback::Callback(){
 }
 
-CallbackDuration::~CallbackDuration(){
+Callback::~Callback(){
 }
 
-CallbackDuration::CallbackDuration(int duration){
-  setDuration(duration);
-}
-
-CallbackDuration::CallbackDuration(int duration, void (* callback)()){
+Callback::Callback(void (* callback)()){
   setCallback(callback);
-  setDuration(duration);
+
 }
 
-void CallbackDuration::setCallback(void (*fn)()){
+void Callback::setCallback(void (*fn)()){
   _callback = fn;
 }
 
-void CallbackDuration::setDuration(int duration){
-  _duration = duration;
-}
-
-void CallbackDuration::activate(){
+void Callback::activate(){
   _active = true;
 }
 
-void CallbackDuration::deactivate(){
+void Callback::deactivate(){
   _active = false;
 }
 
-bool CallbackDuration::isActive(){
+bool Callback::isActive(){
   return _active;
 }
 
-
-void CallbackDuration::run(){
+void Callback::run(){
   _callback();
 }
+
 
 void ConfigButton::setPin(int pin){
   pinMode(pin, INPUT);
   _pin = pin;
 }
 
-void ConfigButton::setCallback(int duration, void (*fn)()){
-  Serial.print("CB::setting up callback for duration ");
-  Serial.println(duration);
-  CallbackDuration callback(duration, fn);
+void ConfigButton::setPressCallback(int duration, void (*fn)()){
+  Serial.print("CB::setting up callback for press of ");
+  Serial.println(duration * PRESS_PERIOD);
+  Callback callback(fn);
   callback.activate();
-  callbackMap.insert(std::make_pair(duration, callback));
+  pressCallbackMap.insert(std::make_pair(duration, callback));
+}
+
+void ConfigButton::setClickCallback(int nClick, void (*fn)()){
+  Serial.print("CB::setting up callback for ");
+  Serial.print(nClick);
+  Serial.print(" clicks");
+  Callback callback(fn);
+  callback.activate();
+  clickCallbackMap.insert(std::make_pair(nClick, callback));
 }
 
 void ConfigButton::setActive(int state ){
   this->_active = state;
 }
 
- ConfigButton::ConfigButton(){
+ConfigButton::ConfigButton(){
 }
 
- ConfigButton::ConfigButton(int pin){
+ConfigButton::ConfigButton(int pin){
   setPin(pin);
 }
 
- ConfigButton::ConfigButton(int pin, int active){
+ConfigButton::ConfigButton(int pin, int active){
   setPin(pin);
   setActive(active);
 }
@@ -78,36 +79,78 @@ void ConfigButton::setActive(int state ){
 ConfigButton::~ConfigButton(){
 }
 
-void ConfigButton::loop(){
-  int pinValue = digitalRead(_pin);
-  if(pinValue == _active){
-    this->_pressDuration = millis()- _pressStart;
+void ConfigButton::readButton(){
+  if(digitalRead(_pin) == _active){
+    _pressing = true;
+    _pressedAt = millis();
+    _pressDuration = _pressedAt - _pressStart;
   }
   else {
-    if (_pressDuration >0){
-      Serial.print("CB::pressed for ");
-      Serial.println(_pressDuration);
-      int value= (int)(_pressDuration/1000);
-      Serial.println("Callback value: ");
-      Serial.print(value);
+    _pressing = false;
+    if (_pressDuration > DEBOUNCE){
+      _lastPressedSince = millis() - _pressedAt;
+      }
+    else{
+      resetPress();
+    }
+  }
+}
 
-      if(runCallback(value)){
+void ConfigButton::resetPress(){
+  _pressStart = millis();
+  _pressDuration = 0;
+  _nClick=0;
+}
+
+void ConfigButton::loop(){
+  readButton();
+  if(!_pressing){
+    if(_pressDuration<= DEBOUNCE){
+      resetPress();
+    }
+    else if (_pressDuration < PRESS_PERIOD){
+      Serial.print("CB::click detected, duration ");
+      Serial.println(_pressDuration);
+      if ( _lastPressedSince < CLICK_TIMEOUT){
+        _nClick++;
+      }
+      else{
+        if(runClickCallback(_nClick)){
+          log("callback ran.");
+        }
+        else{
+          log("couldn't run callback.");
+        }
+        resetPress();
+      }
+    }
+    else {
+      int value = (int)(_pressDuration / PRESS_PERIOD);
+      if(runPressCallback(value)){
         log("callback ran.");
       }
       else{
         log("couldn't run callback.");
       }
+      resetPress();
     }
-    this->_pressStart = millis();
-    this->_pressDuration = 0;
   }
 }
 
-bool ConfigButton:: runCallback(int duration){
-
-  if(callbackMap.find(duration) != callbackMap.end()){
+bool ConfigButton:: runPressCallback(int duration){
+  if(pressCallbackMap.find(duration) != pressCallbackMap.end()){
     Serial.println("Found callback to call");
-    callbackMap[duration].run();
+    pressCallbackMap[duration].run();
+    return true;
+  }
+  Serial.println("Didn't find callback");
+  return false;
+}
+
+bool ConfigButton:: runClickCallback(int nClick){
+  if(clickCallbackMap.find(nClick) != clickCallbackMap.end()){
+    Serial.println("Found callback to call");
+    clickCallbackMap[nClick].run();
     return true;
   }
   Serial.println("Didn't find callback");
